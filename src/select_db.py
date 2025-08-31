47386f049fcd655fb871678b634fe213e93d386b
matheus.santos <matheus.santos@grupo3778.com.br>
2025-08-30 22:39:22 -0300

    feat: get data from the DB

---
+++ src/select_db.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import psycopg
+
+with open('access/pswrd', 'r') as file:
+    pswrd = file.readlines()[0].strip()
+
+# Conexão
+conn = psycopg.connect(
+    host="localhost",
+    dbname="postgres",
+    user="postgres",
+    password=pswrd
+)
+
+# Cursor SQL
+cur = conn.cursor()
+
+# Consulta
+cur.execute("SELECT * FROM tweets;")
+
+# Resultados
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
