18b68c6a032f85b4173c8500fc739d3ed97ef8b5
matheus.santos <matheus.santos@grupo3778.com.br>
2025-08-30 22:39:31 -0300

    feat: insert data in the DB

---
+++ src/insert_db.py
@@ -0,0 +1,27 @@
+import psycopg
+import pandas as pd
+
+with open('access/pswrd', 'r') as file:
+    pswrd = file.readlines()[0].strip()
+
+def insert_data(data, conn_params, table_name):
+    with psycopg.connect(**conn_params) as conn:
+        with conn.cursor() as cur:
+            placeholders = ", ".join(["%s"] * len(data.columns))
+            insert_sql = f"INSERT INTO {table_name} VALUES ({placeholders})"
+            cur.executemany(insert_sql, data.values.tolist())
+        
+        conn.commit()
+
+conn_params = {
+    "host": "localhost",
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": pswrd
+}
+
+data = pd.read_csv('data/tweets_01.csv')
+
+print('Inserindo dados')
+insert_data(data, conn_params, 'tweets')
+print('Dados inseridos')
